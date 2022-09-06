8b6eaf2e55f1e65f68008fef47bea5167bd20aaa
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-09-06 19:23:26 -0400

    commit tree c53ffa5245e6daf69c6c72ef5207a5fa1b7a3bb9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arrNames, eventName){
+    let arr = [];
+    for(let i = 0; i < arrNames.length; i++){
+        arr.push(`Thank you, ${arrNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arr;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n--);
+    }
+}
